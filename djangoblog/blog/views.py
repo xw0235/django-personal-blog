4239b4770d13b1e101d12b7ce77b81709537a1f2
John Fu <fujohn2@hotmail.com>
2018-07-20 13:49:33 -0700

    set up inital base url and view

--- djangoblog/blog/views.py
+++ djangoblog/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+#views connect backend to frontend, allows python code to move to static HTML
+
+def post_list(request):
+	#renders the html which displays 
+	return render(request, 'blog/base.html', {})
--- djangoblog/mysite/urls.py
+++ djangoblog/mysite/urls.py
@@ -14,14 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls import url
 from django.views.static import serve
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #url(r'', include('blog.urls')),
+    #include the urls in blog
+    url(r'', include('blog.urls')),
 
 ] 
 
